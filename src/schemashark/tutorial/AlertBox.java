791c93e300e47b1c24d6a70c0c2377677e26e51c
nwbruce <nwbruce@gmail.com>
2017-10-01 18:22:47 -0400

    Basic GUI layout, exit functionality

--- src/schemashark/tutorial/AlertBox.java
+++ src/schemashark/tutorial/AlertBox.java
@@ -1,35 +1,66 @@
 package schemashark.tutorial;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class AlertBox {
-
-	public static void display(String title, String message) {
-		Stage window = new Stage();
+	private int result = 0;
+	private Stage window;
+	private AlertBox(String title, String message, String... buttons) {
+		window = new Stage();
 		
 		// set modal
 		window.initModality(Modality.APPLICATION_MODAL);
+		window.initStyle(StageStyle.UTILITY);
+		window.setResizable(false);
 		window.setTitle(title);
-		window.setMinWidth(250);
 		
 		Label label = new Label();
 		label.setText(message);
+		label.setWrapText(false);
 		
-		Button closeButton = new Button("OK");
-		closeButton.setOnAction(e -> window.close());
-		
+		HBox buttonLayout = new HBox(10);
+		buttonLayout.setAlignment(Pos.CENTER);
+
+		int counter = 0;
+		for (String lbl : buttons) {
+			final int index = ++counter;
+			Button b = new Button(lbl);
+			b.setOnAction(e -> {
+				result = index;
+				window.close();
+			});
+			buttonLayout.getChildren().add(b);
+		}
+				
 		VBox layout = new VBox(10);
-		layout.getChildren().addAll(label, closeButton);
+		layout.getChildren().addAll(label, buttonLayout);
 		layout.setAlignment(Pos.CENTER);
+		
+		layout.setPadding(new Insets(10));
+				
 		
 		Scene panel = new Scene(layout);
 		window.setScene(panel);
+		
+		
+	}
+	
+	private int show() {
 		window.showAndWait();
+		return result;
+	}
+	
+	public static int display(String title, String message, String... buttons) {
+		AlertBox ab = new AlertBox(title, message, buttons);
+		return ab.show();
 	}
 }
--- src/schemashark/tutorial/Main.java
+++ src/schemashark/tutorial/Main.java
@@ -2,36 +2,108 @@ package schemashark.tutorial;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	Stage window;
-	Button button;
+	TextArea textArea;
+	String loadedDocumentName;
+	boolean documentIsDirty;
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private MenuBar buildMenuBar() {
+		/* File Menu */
+		Menu fileMenu = new Menu("_File");
+		
+		MenuItem fileNewMI = new MenuItem("_New");
+		fileNewMI.setAccelerator(KeyCombination.keyCombination("Shortcut+N"));
+		fileNewMI.setOnAction(e -> textArea.clear());
+		fileMenu.getItems().add(fileNewMI);
+		
+		MenuItem fileOpenMI = new MenuItem("_Open...");
+		fileOpenMI.setAccelerator(KeyCombination.keyCombination("Shortcut+O"));
+		fileMenu.getItems().add(fileOpenMI);
+		
+		MenuItem fileSaveMI = new MenuItem("_Save");
+		fileSaveMI.setAccelerator(KeyCombination.keyCombination("Shortcut+S"));
+		fileMenu.getItems().add(fileSaveMI);
+		
+		MenuItem fileSaveAsMI = new MenuItem("Save _As...");
+		fileMenu.getItems().add(fileSaveAsMI);
+		
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		
+		MenuItem fileExitMI = new MenuItem("E_xit");
+		fileExitMI.setOnAction(e -> tryClose());
+		fileMenu.getItems().add(fileExitMI);
+
+		/* Edit Menu */
+		Menu editMenu = new Menu("_Edit");
+		
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu, editMenu);
+		return menuBar;
+	}
+	
+	private void updateWindowTitle() {
+		StringBuilder sb = new StringBuilder();
+		if (documentIsDirty) {
+			sb.append('*');
+		}
+		sb.append(loadedDocumentName);
+		sb.append(" - Schema Shark");
+		window.setTitle(sb.toString());
+	}
+	
+	private void tryClose() {
+		if (documentIsDirty) {
+			int result = AlertBox.display("SchemaShark", "Do you want to save changes to " + loadedDocumentName + "?", "Save", "Don't Save", "Cancel");
+			if (result == 0 || result == 3) {
+				// cancel
+			} else {
+				window.close();
+			}
+		} else {
+			window.close();
+		}
 	}
 
 	@Override
 	public void start(Stage w) throws Exception {
 		window = w;
-		window.setTitle("SchemaShark");
 		window.setOnCloseRequest(e -> {
 			e.consume();
-			// conditionally...
-			window.close();
+			tryClose();
 		});
 		
-		button = new Button("Click me");
-		button.setOnAction(e -> AlertBox.display("SchemaShark", "The button was clicked"));
+		documentIsDirty = false;
+		loadedDocumentName = "Untitled";
+		updateWindowTitle();
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		MenuBar menuBar = buildMenuBar();
 		
-		Scene panel = new Scene(layout, 300, 250);
+		textArea = new TextArea();
+		textArea.setOnKeyTyped(e -> {
+			documentIsDirty = true;
+			updateWindowTitle();
+		});
+		
+		BorderPane layout = new BorderPane();
+		layout.setTop(menuBar);
+		layout.setCenter(textArea);
+		
+		Scene panel = new Scene(layout, 600, 500);
 		
 		window.setScene(panel);
 		window.show();
