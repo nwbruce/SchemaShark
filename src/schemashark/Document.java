c09081777a55dfde89f85b018e70f02eb019d57b
nwbruce <nwbruce@gmail.com>
2018-02-04 15:47:13 -0500

    Add SaveAs and Tab control

--- src/schemashark/Document.java
+++ src/schemashark/Document.java
@@ -1,11 +1,16 @@
 package schemashark;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Document {
 	private boolean isDirty = false;
-	private String path = null;
+	private File file = null;
 	private String shortName = "Untitled";
 
 	public void setDirty() {
@@ -16,14 +21,44 @@ public class Document {
 		return isDirty;
 	}
 	
-	public void setPath(String p) {
-		path = p;
-		
-		Path pathObj = Paths.get(path);
-		shortName = pathObj.getFileName().toString();
+	public void setFile(File f) {
+		file = f;
+		shortName = file.getName();
 	}
 	
 	public String getShortName() {
 		return shortName;
+	}
+	
+	public boolean hasFileName() {
+		return file != null;
+	}
+	
+	public String read() throws FileNotFoundException, IOException {
+		try(BufferedReader br = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			return sb.toString();
+		}
+	}
+	
+	public void write(String text) throws IOException {
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsolutePath()))) {
+			bw.write(text);
+		}
+		isDirty = false;
+	}
+	
+	public String[] getEmptyXsd() {
+		String[] result = new String[2];
+		result[0] = "<?xml version=\"1.0\"?>\n"
+				+ "<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n";
+		result[1] = "\n</xs:schema>\n";
+		return result;
 	}
 }
--- src/schemashark/Main.java
+++ src/schemashark/Main.java
@@ -1,13 +1,17 @@
 package schemashark;
 
 import java.io.File;
+import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
@@ -24,6 +28,14 @@ public class Main extends Application {
 		launch(args);
 	}
 	
+	private void fileNew() {
+		if (!tryUnloadDocument()) {
+			return;
+		}
+		document = new Document();
+		resetTextAreaToDefault();
+	}
+	
 	private void fileOpen() {
 		if (!tryUnloadDocument()) {
 			return;
@@ -36,7 +48,50 @@ public class Main extends Application {
 		);
 		File f = dialog.showOpenDialog(window);
 		if (f != null) {
-			System.out.println(f.getName());
+			document.setFile(f);
+			try {
+				textArea.setText(document.read());
+				updateWindowTitle();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void fileSaveAs() {
+		FileChooser dialog = new FileChooser();
+		dialog.setTitle("Save As");
+		dialog.getExtensionFilters().addAll(
+				new FileChooser.ExtensionFilter("XML Schemas", "*.xsd"),
+				new FileChooser.ExtensionFilter("All Files", "*")
+		);
+		File f = dialog.showSaveDialog(window);
+		if (f != null) {
+			document.setFile(f);
+			try {
+				document.write(textArea.getText());
+				updateWindowTitle();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private void fileSave() {
+		if (document.hasFileName()) {
+			try {
+				document.write(textArea.getText());
+				updateWindowTitle();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			// save as...
+			fileSaveAs();
 		}
 	}
 	
@@ -46,7 +101,7 @@ public class Main extends Application {
 		
 		MenuItem fileNewMI = new MenuItem("_New");
 		fileNewMI.setAccelerator(KeyCombination.keyCombination("Shortcut+N"));
-		fileNewMI.setOnAction(e -> textArea.clear());
+		fileNewMI.setOnAction(e -> fileNew());
 		fileMenu.getItems().add(fileNewMI);
 		
 		MenuItem fileOpenMI = new MenuItem("_Open...");
@@ -56,9 +111,11 @@ public class Main extends Application {
 		
 		MenuItem fileSaveMI = new MenuItem("_Save");
 		fileSaveMI.setAccelerator(KeyCombination.keyCombination("Shortcut+S"));
+		fileSaveMI.setOnAction(e -> fileSave());
 		fileMenu.getItems().add(fileSaveMI);
 		
 		MenuItem fileSaveAsMI = new MenuItem("Save _As...");
+		fileSaveAsMI.setOnAction(e -> fileSaveAs());
 		fileMenu.getItems().add(fileSaveAsMI);
 		
 		fileMenu.getItems().add(new SeparatorMenuItem());
@@ -90,6 +147,7 @@ public class Main extends Application {
 			int result = AlertBox.display("SchemaShark", "Do you want to save changes to " + document.getShortName() + "?", "Save", "Don't Save", "Cancel");
 			if (result == 1) {
 				// save it
+				fileSave();
 				return true;
 			} else if (result == 2) {
 				// don't save it
@@ -107,6 +165,14 @@ public class Main extends Application {
 		if (tryUnloadDocument()) {
 			window.close();
 		}
+	}
+	
+	private void resetTextAreaToDefault() {
+		String[] pair = document.getEmptyXsd();
+		textArea.setText(pair[0]);
+		textArea.appendText(pair[1]);
+		textArea.requestFocus();
+		textArea.selectRange(pair[0].length(), pair[0].length());
 	}
 
 	@Override
@@ -128,10 +194,32 @@ public class Main extends Application {
 			document.setDirty();
 			updateWindowTitle();
 		});
+		textArea.setStyle("-fx-font-family: Consolas;");
+		resetTextAreaToDefault();
+		
+		Tab tab1 = new Tab();
+		tab1.setText("Source");
+		tab1.setContent(textArea);
+		tab1.setClosable(false);
+
+		Tab tab2 = new Tab();
+		tab2.setText("Edit");
+		tab2.setClosable(false);
+		
+		Tab tab3 = new Tab();
+		tab3.setText("Browse");
+		tab3.setClosable(false);
+		
+		TabPane tabPane = new TabPane();
+		tabPane.setSide(Side.LEFT);
+		tabPane.getTabs().add(tab1);
+		tabPane.getTabs().add(tab2);
+		tabPane.getTabs().add(tab3);
+		
 		
 		BorderPane layout = new BorderPane();
 		layout.setTop(menuBar);
-		layout.setCenter(textArea);
+		layout.setCenter(tabPane);
 		
 		Scene panel = new Scene(layout, 600, 500);
 		
